9a948506c59add3df2bc0c142e2ab282745bc897
Siddhartha RT <sddhrthrt@gmail.com>
2012-06-13 15:50:17 +0530

    Version 1.0 :D

--- flaskr.py
+++ flaskr.py
@@ -16,7 +16,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from flask_login import LoginManager
 # configuration
-DATABASE = '/srv/www/blogsite/flaskr.db'
+DATABASE = '/srv/www/blogsite/data/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -29,48 +29,54 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
-    """Returns a new connection to the database."""
-    return sqlite3.connect(app.config['DATABASE'])
+	"""Returns a new connection to the database."""
+	return sqlite3.connect(app.config['DATABASE'])
 
 
 def init_db():
-    """Creates the database tables."""
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
+	"""Creates the database tables."""
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+@app.route('/approve')
+def approve():
+	if not session.get('logged_in'):
+		abort(401)
+	quotes=[]
+	with open("data/pending.txt", 'r') as f:
+		lines=f.readlines()	
+		for i in range(len(lines)/4):
+			quotes+=[dict(quote=lines[i*4], author=lines[i*4+1], tags=lines[i*4+2], sender=lines[i*4+3])]
+	return render_template('approve.html', quotes=quotes)
 
 @app.before_request
 def before_request():
-    """Make sure we are connected to the database each request."""
-    g.db = connect_db()
-
-
+	"""Make sure we are connected to the database each request."""
+	g.db = connect_db()
 @app.teardown_request
 def teardown_request(exception):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'db'):
-        g.db.close()
+	"""Closes the database again at the end of the request."""
+	if hasattr(g, 'db'):
+		g.db.close()
 def getAllTags():
-    cur = g.db.execute('select tags from entries order by id desc')
-    taglist=[]
-    for row in cur:
-    	for tag in row[0].split(','):
-			print tag
+	cur = g.db.execute('select tags from entries order by id desc')
+	taglist=[]
+	for row in cur:
+		for tag in row[0].split(','):
 			if tag not in taglist:
 				taglist+=[tag]
-    return taglist
+	return taglist
 
 def getAllQuotes():
-    cur = g.db.execute('select id, quote, author, tags, sender from entries order by id desc')
-    return [dict(id=row[0], quote=row[1], author=row[2], tags=row[3].split(','), sender=row[4]) for row in cur.fetchall()]
+	cur = g.db.execute('select id, quote, author, tags, sender from entries order by id desc')
+	return [dict(id=row[0], quote=row[1], author=row[2], tags=row[3].split(','), sender=row[4]) for row in cur.fetchall()]
 
 @app.route('/')
 def show_entries():
-    q=getAllQuotes()
-    data = dict(quotes=q, tags=getAllTags(), add=False)
-    return render_template('layout.html', data=data)
+	q=getAllQuotes()
+	data = dict(quotes=q, tags=getAllTags(), add=False)
+	return render_template('layout.html', data=data)
 
 def getQuotes(tag):
 	cur = g.db.execute('select id, quote, author, tags, sender from entries order by id desc')
@@ -78,55 +84,71 @@ def getQuotes(tag):
 
 @app.route('/by-tag/<tag>')
 def show_tag_entries(tag):
-    q=getQuotes(tag)
-    data = dict(quotes=q, tags=getAllTags(), add=False)
-    return render_template('layout.html', data=data)
+	q=getQuotes(tag)
+	data = dict(quotes=q, tags=getAllTags(), add=False)
+	return render_template('layout.html', data=data)
 
 @app.route('/add')
 def add():
-    q=getAllQuotes()
-    data = dict(quotes=q, tags=getAllTags(), add=True)
-    return render_template('layout.html', data=data)
+	q=getAllQuotes()
+	data = dict(quotes=q, tags=getAllTags(), add=True)
+	return render_template('layout.html', data=data)
 
-@app.route('/submit', methods=['GET', 'POST'])
+@app.route('/submit', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    if(len(request.form['quote'])==0):
-    	flash("Entry failed")
+	if not session.get('logged_in'):
+		abort(401)
+	if(len(request.form['quote'])==0):
+		flash("Entry failed")
+		return redirect(url_for('show_entries'))
+
+	with open("data/pending.txt", 'a') as f:
+		f.write(request.form['quote']+'\n')
+		f.write(request.form['author']+'\n')
+		f.write(request.form['tags']+'\n')
+		f.write(USERNAME+'\n')
 	return redirect(url_for('show_entries'))
-    if(len(request.form['author'])==0):
-	    g.db.execute('insert into entries (quote, author, tags, sender) values (?, ?, ?, ?)',
-                 [request.form['quote'], "Anon.", request.form['tags'], USERNAME])
-    else:
-	    g.db.execute('insert into entries (quote, author, tags, sender) values (?, ?, ?, ?)',
-                 [request.form['quote'], request.form['author'], request.form['tags'], USERNAME])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+@app.route('/approved', methods=['POST'])
+def approved():
+	with open("data/pending.txt", "r") as f:
+		a=f.readlines()
+		print a
+	for i in request.form:
+		i=int(i)
+		if(len(a[i*4+1])==0):
+			g.db.execute('insert into entries (quote, author, tags, sender) values (?, ?, ?, ?)', [a[i*4], "Anon.", a[i*4+2], a[i*4+3]])
+		else:
+			g.db.execute('insert into entries (quote, author, tags, sender) values (?, ?, ?, ?)', [a[i*4], a[i*4+1], a[i*4+2], a[i*4+3]])
+		g.db.commit()
+		del a[i*4:i*4+4]
+		flash('New entry was successfully posted')
+	with open("data/pending.txt", "w") as f:
+		f.writelines(a)
+		print a
+		return redirect(url_for('show_entries'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['Email'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['Password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login_error.html', error=error)
+	error = None
+	if request.method == 'POST':
+		if request.form['Email'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['Password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login_error.html', error=error)
 
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
